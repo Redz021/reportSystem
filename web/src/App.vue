5442c78c96aaa221f362364c250f201080f1423a
Red <13191648829@163.com>
2021-03-05 20:18:58 +0800

    pull测试

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div>this is a test</div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
